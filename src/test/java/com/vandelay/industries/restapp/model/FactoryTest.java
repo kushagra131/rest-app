d640f351e987d54c1036604a00c26302a69ce696
Kushagra Sharma <kushagra.sharma131@gmail.com>
2025-11-30 12:05:18 -0500

    Updated the Spring Boot to 3.5.7 and added JPA Test for functionality

---
+++ src/test/java/com/vandelay/industries/restapp/model/FactoryTest.java
@@ -0,0 +1,65 @@
+package com.vandelay.industries.restapp.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//@DataJpaTest
+class FactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void addMachine() {
+    }
+
+    @Test
+    void removeMachine() {
+    }
+
+    @Test
+    void getFactoryId() {
+    }
+
+    @Test
+    void getFactoryName() {
+    }
+
+    @Test
+    void getFactoryDescription() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getMachines() {
+    }
+
+    @Test
+    void setFactoryId() {
+    }
+
+    @Test
+    void setFactoryName() {
+    }
+
+    @Test
+    void setFactoryDescription() {
+    }
+
+    @Test
+    void setMachines() {
+    }
+}
